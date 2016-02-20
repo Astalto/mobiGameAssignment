bbfccfdddb6c6008f330350c114a644039d45b01
aytona <christopher.aytona@gmail.com>
2016-02-19 23:42:46 -0500

    Moving background + added more background imgs

---
+++ Assets/Scripts/BackgroundMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMovement : MonoBehaviour {
+
+    [Tooltip("Speed of the background/game")]
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+    }
+}
