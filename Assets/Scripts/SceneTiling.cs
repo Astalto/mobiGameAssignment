a2a5c22e5b90f04dd7bb44f468732cfd930a77b2
Astalto <gowaoe@gmail.com>
2016-02-22 10:58:00 -0500

    Added partial scrolling

---
+++ Assets/Scripts/SceneTiling.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTiling : MonoBehaviour {
+    public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().sortingOrder = -1;
+        Debug.Log(GetComponent<Renderer>().sortingLayerName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 bgOffset = new Vector2(Time.time * speed, 0);
+
+        GetComponent<Renderer>().material.mainTextureOffset = bgOffset;
+	}
+}
